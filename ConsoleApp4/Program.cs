9f20e05db7a3b685ac675bac79eedf3fad18becc
MPRG2122 <oficinacontabilidadalicante@gmail.com>
2022-03-15 20:05:03 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ichingMPRG2122
+
+{
+    class Moneda
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MPRG2122");
+            JuegoMonedas j = new JuegoMonedas();
+            j.Consultar();
+        }
+        private int valor;
+        private static Random azar; /*declaro static valor = all objetos d sta clase*/
+                                    /* la clase random genera nºs pseudo-aleatorios*/
+
+        public Moneda()
+        {
+            azar = new Random();
+        }
+
+        public void Tirar()
+        {
+            valor = azar.Next(1, 6); /* .Next devuelve entero aleatorio no -*/
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("La moneda ha salido" + valor);
+        }
+
+        public int DevolverValor()
+        {
+            return valor;
+        }
+    }
+
+    class JuegoMonedas
+    {
+        private Moneda moneda1, moneda2, moneda3;
+
+        public JuegoMonedas()
+            {
+            moneda1 = new Moneda();
+            moneda2 = new Moneda();
+            moneda3 = new Moneda();
+        }
+
+        public void Consultar()
+        {
+            moneda1.Tirar();
+            moneda1.Mostrar();
+            moneda2.Tirar();
+            moneda2.Mostrar();
+            moneda3.Tirar();
+            moneda3.Mostrar();
+        }
+
+    }
+}
